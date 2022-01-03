ee36e0d4019b1cfb8fef8fee9e9e8963bc9451f0
oksa satya <oksasatyaa@gmail.com>
2022-01-03 12:12:16 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+    created(){
+    window.axios.get('http://larashop-api.test/buku/php')
+      .then((response) => {
+          console.log(response.data)
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+    }
+  }
+</script>
